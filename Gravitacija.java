a6294bdd1c29d2b1e18c104db3ca338379c07249
t1rony <seliskaraleksander@gmail.com>
2025-02-24 23:20:19 +0100

    Dodaj izpis pospeška #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,9 @@
 public class Gravitacija {
 
+	public static void izpis(double visina, double gravitacija) {
+		System.out.printf("[%f m] -> [%f m/s^2]\n", visina, gravitacija);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
 	}
